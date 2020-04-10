c8e8f07f9365251e7af4807afd409b3aafcb611f
nailcankara <53586899+nailcankara@users.noreply.github.com>
2020-04-10 18:04:53 +0300

    Update logistic_regression.py

--- 07_LogisticRegression/logistic_regression.py
+++ 07_LogisticRegression/logistic_regression.py
@@ -1,28 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#%%
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values #Choosed only numerical values
 y = dataset.iloc[:, 4].values
-
+#%%
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
+#%%
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
+#%%
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
-
+#%%
 y_pred = classifier.predict(X_test)
-
+#%%
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
+#%%
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
