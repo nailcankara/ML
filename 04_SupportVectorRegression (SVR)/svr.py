74b15437080f7742cf5f8ccb47ef97bcf508f3cb
nailcankara <53586899+nailcankara@users.noreply.github.com>
2020-04-22 22:46:24 +0300

    Update svr.py

--- 04_SupportVectorRegression (SVR)/svr.py
+++ 04_SupportVectorRegression (SVR)/svr.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#%%
+
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
-#%%
+
 from sklearn.preprocessing import StandardScaler  #Svr doesnt include feature scaling
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y.reshape(-1,1))
-#%%
+
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
-#%%
+
 y_pred = regressor.predict([[6.5]])
 y_pred = sc_y.inverse_transform(y_pred)   #due to scaling , inverse transform
-#%%
+
 # Visualising the SVR results
 plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
 plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
@@ -26,7 +26,7 @@ plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-#%%
+
 X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
