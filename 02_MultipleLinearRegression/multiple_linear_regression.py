81555708ef4bec6cb91dd9f5fef9cf4c63b21123
nailcankara <53586899+nailcankara@users.noreply.github.com>
2020-04-22 22:45:33 +0300

    Update multiple_linear_regression.py

--- 02_MultipleLinearRegression/multiple_linear_regression.py
+++ 02_MultipleLinearRegression/multiple_linear_regression.py
@@ -1,35 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#%%
+
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
-#%%
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder   # Encoding categorical data
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
-#%%
+
 X = X[:, 1:]  # Avoiding the Dummy Variable Trap
-#%%
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-#%%
+
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
-#%%
+
 y_pred = regressor.predict(X_test)
-#%%
+
 import statsmodels.formula.api as sm #model selection
 X = np.append(arr=np.ones((50,1)).astype(int) ,values=X , axis = 1)   #Adding x0 as
-#%%
+
 X_opt = X[:,[0,3,5]]   #important features
 regressor_OLS = sm.OLS(endog = y , exog = X_opt).fit()
 regressor_OLS.summary()
-#%%
+
 #Automatic p values
 import statsmodels.formula.api as sm
 def backwardElimination(x, sl):
