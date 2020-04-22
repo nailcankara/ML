2f3c5a2b23bbc6ae9481965a5600ed760d1738aa
nailcankara <53586899+nailcankara@users.noreply.github.com>
2020-04-22 22:47:38 +0300

    Update random_forest_regression.py

--- 06_RandomForestRegression/random_forest_regression.py
+++ 06_RandomForestRegression/random_forest_regression.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#%%
+
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
-#%%
+
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X, y)
-#%%
+
 y_pred = regressor.predict([[6.5]])
-#%%
+
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
