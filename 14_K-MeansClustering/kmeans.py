3a909e3a6369ef44d36700435bf3c938a80146f6
nailcankara <53586899+nailcankara@users.noreply.github.com>
2020-04-22 22:50:33 +0300

    Update kmeans.py

--- 14_K-MeansClustering/kmeans.py
+++ 14_K-MeansClustering/kmeans.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#%%
+
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
-#%%
+
 from sklearn.cluster import KMeans         
 wcss = []  #elbow method
 for i in range(1, 11):
@@ -16,10 +16,10 @@ plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
-#%%
+
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
-#%%
+
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
